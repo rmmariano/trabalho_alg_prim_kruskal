3fc9cf2f3358d2ea5af4d3793810158cb4477cf1
Rodrigo M. Mariano <rodmariano13@gmail.com>
2017-05-19 22:26:57 -0300

    merge file

--- .merge.sh
+++ .merge.sh
@@ -4,7 +4,6 @@
 ## preciso estar na dev_mariano
 
 
-<<COMMENT1
 git pull origin dev_mariano
 git push origin dev_mariano
 
@@ -39,7 +38,7 @@ git push origin master
 
 git merge dev_joamila
 git push origin master
-COMMENT1
+
 
 git checkout dev_mariano
 git merge master
@@ -47,7 +46,6 @@ git push origin dev_mariano
 
 
 # eles
-
 <<COMMENT1
 #git checkout master
 #git pull origin master
